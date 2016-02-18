8fd5a187e7bb4ab938c0acaf49377809f0f49e86
Qi <qizhao2@gmail.com>
2016-02-18 15:02:48 -0800

    added hw2 into github
    This is the coen 276 project 2

---
+++ js/questions.js
@@ -0,0 +1,100 @@
+var geoQuestions = [
+{
+	"question": "Where is the great Barrier Reef?",
+	"questionType": 1,
+	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
+	"correctChoice": "Asutralia",
+	"score": 12
+},
+{
+	"question": "Where is the great Barrier Reef?",
+	"questionType": 1,
+	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
+	"correctChoice": "Asutralia",
+	"score": 12
+},
+{
+	"question": "Where is the great Barrier Reef?",
+	"questionType": 1,
+	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
+	"correctChoice": "Asutralia",
+	"score": 12
+},
+{
+	"question": "Where is the great Barrier Reef?",
+	"questionType": 1,
+	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
+	"correctChoice": "Asutralia",
+	"score": 12
+},
+{
+	"question": "Where is the great Barrier Reef?",
+	"questionType": 1,
+	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
+	"correctChoice": "Asutralia",
+	"score": 12
+},
+{
+	"question": "What planet is Curiosity Rover on now?",
+	"questionType": 2,
+	"correctAnswer": "Mars",
+	"score": 20
+},
+{
+	"question": "What planet is Curiosity Rover on now?",
+	"questionType": 2,
+	"correctAnswer": "Mars",
+	"score": 20
+}];
+
+
+
+
+var wotQuestions = [
+{
+	"question": "Where is the great Barrier Reef?",
+	"questionType": 1,
+	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
+	"correctChoice": "Asutralia",
+	"score": 12
+},
+{
+	"question": "Where is the great Barrier Reef?",
+	"questionType": 1,
+	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
+	"correctChoice": "Asutralia",
+	"score": 12
+},
+{
+	"question": "Where is the great Barrier Reef?",
+	"questionType": 1,
+	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
+	"correctChoice": "Asutralia",
+	"score": 12
+},
+{
+	"question": "Where is the great Barrier Reef?",
+	"questionType": 1,
+	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
+	"correctChoice": "Asutralia",
+	"score": 12
+},
+{
+	"question": "Where is the great Barrier Reef?",
+	"questionType": 1,
+	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
+	"correctChoice": "Asutralia",
+	"score": 12
+},
+{
+	"question": "What planet is Curiosity Rover on now?",
+	"questionType": 2,
+	"correctAnswer": "Mars",
+	"score": 20
+},
+{
+	"question": "What planet is Curiosity Rover on now?",
+	"questionType": 2,
+	"correctAnswer": "Mars",
+	"score": 20
+}];
