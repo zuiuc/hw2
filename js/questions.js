ab526326f2467ac2a08cc369ae3aa4428224ee8a
Qi <qizhao2@gmail.com>
2016-02-27 21:29:18 -0800

    web quiz finished

--- js/questions.js
+++ js/questions.js
@@ -3,7 +3,7 @@ var geoQuestions = [
 	"question": "Where is the great Barrier Reef?",
 	"questionType": 1,
 	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
-	"correctChoice": "Asutralia",
+	"correctChoice": "Australia",
 	"score": 10
 },
 {
@@ -21,10 +21,10 @@ var geoQuestions = [
 	"score": 10
 },
 {
-	"question": "Where is the great Barrier Reef?",
+	"question": "Which country has the biggest land?",
 	"questionType": 1,
-	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
-	"correctChoice": "Asutralia",
+	"choices": ["Korea", "China", "United States", "Russia", "Australia"],
+	"correctChoice": "Russia",
 	"score": 10
 },
 {
@@ -38,14 +38,14 @@ var geoQuestions = [
 	"question": "Where is the Mt. Fuji?",
 	"questionType": 1,
 	"choices": ["Japan", "China", "United States", "Canada", "Australia"],
-	"correctChoice": "Asutralia",
+	"correctChoice": "Japan",
 	"score": 10
 },
 {
-	"question": "Where is the great Barrier Reef?",
+	"question": "Where is Tahoe Lake?",
 	"questionType": 1,
-	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
-	"correctChoice": "Asutralia",
+	"choices": ["Arizona", "Illinois", "Iowa", "Alaska", "California"],
+	"correctChoice": "California",
 	"score": 10
 },
 {
@@ -79,51 +79,80 @@ var geoQuestions = [
 
 var mathQuestions = [
 {
-	"question": "Where is the great Barrier Reef?",
+	"question": "What is 1+1?",
 	"questionType": 1,
-	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
-	"correctChoice": "Asutralia",
-	"score": 12
+	"choices": ["2", "3", "0", "-1", "34"],
+	"correctChoice": "2",
+	"score": 10
 },
 {
-	"question": "Where is the great Barrier Reef?",
+	"question": "What is 23-10?",
 	"questionType": 1,
-	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
-	"correctChoice": "Asutralia",
-	"score": 12
+	"choices": ["12", "13", "20", "-31", "34"],
+	"correctChoice": "13",
+	"score": 10
 },
 {
-	"question": "Where is the great Barrier Reef?",
+	"question": "What is 45+30?",
 	"questionType": 1,
-	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
-	"correctChoice": "Asutralia",
-	"score": 12
+	"choices": ["75", "3", "0", "-1", "34"],
+	"correctChoice": "75",
+	"score": 10
 },
 {
-	"question": "Where is the great Barrier Reef?",
+	"question": "What is 4 x 4?",
 	"questionType": 1,
-	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
-	"correctChoice": "Asutralia",
-	"score": 12
+	"choices": ["12", "13", "10", "16", "34"],
+	"correctChoice": "16",
+	"score": 10
 },
 {
-	"question": "Where is the great Barrier Reef?",
+	"question": "What is 2 x 16?",
 	"questionType": 1,
-	"choices": ["Africa", "N.America", "S.America", "Asia", "Australia"],
-	"correctChoice": "Asutralia",
-	"score": 12
+	"choices": ["23", "32", "777", "-1", "34"],
+	"correctChoice": "32",
+	"score": 10
 },
 {
-	"question": "What planet is Curiosity Rover on now?",
+	"question": "What is 9 x 9?",
+	"questionType": 1,
+	"choices": ["321", "231", "0", "81", "4"],
+	"correctChoice": "81",
+	"score": 10
+},
+{
+	"question": "What is 8*8/8/8?",
+	"questionType": 1,
+	"choices": ["24", "32", "23", "1", "75"],
+	"correctChoice": "1",
+	"score": 10
+},
+{
+	"question": "What is 2^3?",
+	"questionType": 1,
+	"choices": ["2", "8", "0", "72", "42"],
+	"correctChoice": "8",
+	"score": 10
+},
+{
+	"question": "What is 67-4?",
+	"questionType": 1,
+	"choices": ["63", "67", "21", "-1", "34"],
+	"correctChoice": "63",
+	"score": 10
+},
+{
+	"question": "what is 12 x 12?",
 	"questionType": 2,
-	"correctAnswer": "Mars",
-	"score": 20
+	"correctAnswer": "144",
+	"score": 10
 },
+
 {
-	"question": "What planet is Curiosity Rover on now?",
+	"question": "what is 15 x 15?",
 	"questionType": 2,
-	"correctAnswer": "Mars",
-	"score": 20
+	"correctAnswer": "225",
+	"score": 10
 }];
 
 
@@ -135,45 +164,73 @@ var wotQuestions = [
 	"questionType": 1,
 	"choices": ["6", "7", "8", "9", "10"],
 	"correctChoice": "7",
-	"score": 12
+	"score": 10
 },
 {
 	"question": "What type of tank is American T29 tank",
 	"questionType": 1,
 	"choices": ["SPG", "Heavy Tank", "Tank Destroyer", "Medium Tank", "Light Tank"],
 	"correctChoice": "Heavy Tank",
-	"score": 12
+	"score": 10
 },
 {
 	"question": "What is the best gun can be mounted on the IS-3?",
 	"questionType": 1,
 	"choices": ["BL-9", "BL-10", "BL-7", "LB-1", "D-54"],
 	"correctChoice": "BL-9",
-	"score": 12
+	"score": 10
 },
 {
 	"question": "Where is thickness of front turret armor of T-54?",
 	"questionType": 1,
 	"choices": ["190mm", "50mm", "200mm", "240mm", "170mm"],
 	"correctChoice": "200mm",
-	"score": 12
+	"score": 10
 },
 {
 	"question": "Where is the alpha damage of the AT gun T7 mounted on T30?",
 	"questionType": 1,
 	"choices": ["256", "276", "311", "750", "560"],
 	"correctChoice": "750",
-	"score": 12
+	"score": 10
 },
 {
 	"question": "What tier is American Tank Destryer T30?",
 	"questionType": 2,
 	"correctAnswer": "9",
-	"score": 20
+	"score": 10
+},
+{
+	"question": "What tier is American T20 Tank?",
+	"questionType": 1,
+	"choices": ["6", "7", "8", "9", "10"],
+	"correctChoice": "7",
+	"score": 10
+},
+{
+	"question": "What tier is American T29 Tank?",
+	"questionType": 1,
+	"choices": ["6", "7", "8", "9", "10"],
+	"correctChoice": "7",
+	"score": 10
+},
+{
+	"question": "What tier is American T110E5 Tank?",
+	"questionType": 1,
+	"choices": ["6", "7", "8", "9", "10"],
+	"correctChoice": "10",
+	"score": 10
+},
+{
+	"question": "What tier is Russian T54 Tank?",
+	"questionType": 1,
+	"choices": ["6", "7", "8", "9", "10"],
+	"correctChoice": "9",
+	"score": 10
 },
 {
 	"question": "Does Chinese tech tree in World of tanks has a SPG line?",
 	"questionType": 2,
 	"correctAnswer": "No",
-	"score": 20
+	"score": 10
 }];
--- js/quizBuilder.js
+++ js/quizBuilder.js
@@ -1,4 +1,4 @@
-
+"use strict";
   //$(document).ready(function()) {
 
   //});
@@ -6,14 +6,13 @@
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
+        minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
         display.textContent = "Your Quiz End in " + minutes + ":" + seconds + " Minutes";
-
         if (--timer < 0) {
             display.textContent = "Quiz Time is Over!!!!";
         }
@@ -26,15 +25,17 @@ function startTimer(duration, display) {
 var d = new Date();
 
 window.onload = function () {
-    var quizTime = 60 * 50,
-        display = document.querySelector('#counter');
-    startTimer(quizTime, display);
     $("#date").html(d.toDateString());
 };
 
-  var questionCounter = 0; 
+
+
+
+  var selection=[];
+  var questionCounter = 0;
+  var total_score = 0;
   var correct_count = 0;
-  var wrong_count = 0; 
+  var wrong_count = 0;
   var quiz = $('#quiz');
   var question;
 
@@ -49,10 +50,20 @@ function select_question() {
     } else {
         question = wotQuestions;
     }
-    console.log(JSON.stringify(shuffle(question)));
-};
+}
 
     select_question();
+
+$(document).ready(function(){
+      if(question == mathQuestions){
+        $('#cat').append("Math");
+      } else if (question == geoQuestions){
+        $('#cat').append("Geography");
+      } else {
+        $('#cat').append("World of Tanks");
+      }
+});
+
 
 // The following code is cited from stck-overflow http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
@@ -74,6 +85,187 @@ function shuffle(array) {
   return array;
 }
 
+question = shuffle(question);
+
+//create_question, user choice and create_choices was modified from http://codepen.io/gcarino/pen/LDgtn/
+function create_question(number){
+  var qElement = $('<div>', {
+      id: 'problem'
+    });
+
+    var head = $('<h2>Question ' + (number + 1) + ':</h2>');
+    qElement.append(head);
+
+    var questions = $('<p>').append(question[number].question);
+    qElement.append(questions);
+
+    return qElement;
+
+}
+
+function create_choices(number){
+    var choices = $('<ul>');
+    var each;
+    var input = '';
+    for (var i = 0; i < question[number].choices.length; i++) {
+      each = $('<li class = "choices">');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += question[number].choices[i];
+      each.append(input);
+      choices.append(each);
+    }
+    return choices;
+
+}
+
+function create_inputbox(){
+
+  var input = $('<div>');
+  input.append('<input name="question" type="text"/>');
+  return input;
+}
+
+function user_choose() {
+  selection[questionCounter] = +$('input[name="answer"]:checked').val();
+}
+
+function user_input() {
+
+  selection[questionCounter] = $('input[name="question"]').val();
+}
+
+function displayFirst() {
+    if(question[0].questionType == 1){
+      var firstQuestion = create_question(0);
+      $("#quiz").append(firstQuestion);
+      var first_choices = create_choices(0);
+      $("#quiz").append(first_choices);
+
+    } else {
+      var firstQuestion = create_question(0);
+      $("#quiz").append(firstQuestion);
+      var first_choices = create_inputbox;
+      $("#quiz").append(first_choices);
+    }
+}
+
+function displayNext() {
+      $('#quiz').empty();
+      if(questionCounter < question.length){
+        if(question[questionCounter].questionType == 1){
+          var nextQuestion = create_question(questionCounter);
+          $("#quiz").append(nextQuestion);
+          var next_choices = create_choices(questionCounter);
+          $("#quiz").append(next_choices);
+
+        } else {
+          var nextQuestion = create_question(questionCounter);
+          $("#quiz").append(nextQuestion);
+          var next_choices = create_inputbox;
+          $("#quiz").append(next_choices);
+        }
+      } else {
+        $('#quiz').append('<h3>Thank you for taking this quiz with us!</h3>');
+        displayFinalScore();
+        $('#next').hide();
+        $('.timer').empty();
+        $('.timer').append("<h2>Congratulations!!</h2>");
+      }
+}
+
+function displayFinalScore() {
+      $('#score').empty();
+      $('#correct').empty();
+      $('#wrong').empty();
+      $('#score').append(total_score);
+      $('#correct').append(correct_count);
+      $('#wrong').append(wrong_count);
+
+}
+
+function updateScore() {
+
+        if(question[questionCounter].questionType == 1){
+          //console.log(selection[questionCounter]);
+          //console.log(question[questionCounter].correctChoice);
+          if(selection[questionCounter] === question[questionCounter].choices.indexOf(question[questionCounter].correctChoice)){
+          total_score += question[questionCounter]. score;
+          correct_count ++;
+          } else {
+           wrong_count ++;
+          }
+        } else {
+          //console.log(selection[questionCounter]);
+          //console.log(questionCounter);
+          //console.log(question[questionCounter].correctChoice);
+          //console.log(question[10].correctChoice);
+          if(selection[questionCounter] === question[questionCounter].correctAnswer){
+          total_score += question[questionCounter]. score;
+          correct_count ++;
+          } else {
+           wrong_count ++;
+          }
+        }
 
 
-    
+
+
+
+      $('#score').empty();
+      $('#correct').empty();
+      $('#wrong').empty();
+      $('#score').append(total_score);
+      $('#correct').append(correct_count);
+      $('#wrong').append(wrong_count);
+}
+
+
+$(document).ready(function(){
+      $('#start').on('click', function (event) {
+        $("html, body").animate({ scrollTop: $(document).height() });
+        var quizTime = 60 * 10,
+        display = document.querySelector('#counter');
+        startTimer(quizTime, display);
+        $('#start').hide();
+        $('#press_start').hide();
+        $('#score_section').show();
+        $('#next').show();
+        displayFirst();
+        });
+});
+
+$(document).ready(function(){
+      $('#next').on('click', function (event) {
+        $("html, body").animate({ scrollTop: $(document).height() });
+        if(question[questionCounter].questionType == 1){
+          user_choose();
+        } else {
+          user_input();
+        }
+        if(question[questionCounter].questionType == 1){
+              if (isNaN(selection[questionCounter])) {
+              alert('Please choose an answer!');
+            } else {
+              updateScore();
+              questionCounter++;
+              displayNext();
+            }
+        } else {
+           if (!selection[questionCounter] || selection[questionCounter].length == 0 ) {
+              alert('Please answer the question!');
+            } else {
+              updateScore();
+              questionCounter++;
+              displayNext();
+            }
+        }
+
+
+        });
+});
+
+$(document).ready(function(){
+      $('#quit').on('click', function (event) {
+          location.reload();
+        });
+});
